fe5f495ba1407e113a07702203775403182a047b
peri-7 <pericles.alexiou@gmail.com>
2025-05-01 23:18:45 +0300

    Update Q03.sql

--- Queries/Q03.sql
+++ Queries/Q03.sql
@@ -5,7 +5,7 @@ SELECT
 	COUNT(*) AS warmup_count
 FROM Performance p
 JOIN Performer pf ON p.performer_id = pf.performer_id
-JOIN Event e ON e.event_id=p.event_id
+JOIN Event_P e ON e.event_id=p.event_id
 WHERE p.type_id = 'warm-up'
 GROUP BY pf.performer_id, e.festival_id
 HAVING COUNT(*) >= 3
