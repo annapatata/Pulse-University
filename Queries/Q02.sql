5806658ba46de0020ff82c558ca915c85e0d1b90
peri-7 <pericles.alexiou@gmail.com>
2025-05-01 23:15:23 +0300

    Update Q02.sql

--- Queries/Q02.sql
+++ Queries/Q02.sql
@@ -2,6 +2,6 @@ SELECT p.performer_name,
 	IF(py.year IS NOT NULL, 'YES', 'NO') AS Participated
 FROM Performer p 
 JOIN PerformerSubgenre ps ON p.performer_id = ps.performer_id
-JOIN Subgenre s ON p.subgenre_id = s.subgenre_id
+JOIN Subgenre s ON ps.subgenre_id = s.subgenre_id
 LEFT JOIN PerformerYear py ON p.performer_id = py.performer_id AND py.year_id = 2025
 WHERE s.genre = 'Rock';
