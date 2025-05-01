79cb569c8536e11bd4e6684084d4e3d80bad4b12
annapatata <142344652+annapatata@users.noreply.github.com>
2025-05-02 01:23:16 +0300

    Update Q01.sql

--- Queries/Q01.sql
+++ Queries/Q01.sql
@@ -1,4 +1,4 @@
 SELECT YEAR(purchase_date) AS purchase_year, SUM(price) AS total_revenue, pay_method
-FROM TICKETS
+FROM Ticket
 GROUP BY purchase_year, pay_method
 ORDER BY purchase_year,pay_method;
