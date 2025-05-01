2c2c8628548826b944d17c717fdd0c9ba61914e9
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-01 17:40:38 +0300

    Add files via upload

---
+++ Queries/Q10.sql
@@ -0,0 +1,19 @@
+SELECT 
+	s1.genre_id AS Genre_1,
+	s2.genre_id AS Genre_2,
+	COUNT(*) AS Artists
+FROM 
+	(SELECT DISTINCT
+		ps.performer_id,
+		s.genre_id
+	FROM PerformerSubgenre ps 
+	JOIN Subgenre s ON ps.subgenre_id = s.subgenre_id
+	)AS s1
+JOIN s1 AS s2 ON s1.performer_id = s2.performer_id
+WHERE s1.genre_id < s2.genre_id
+GROUP BY 
+	s1.genre_id, 
+	s2.genre_id 
+ORDER BY 
+	COUNT(*) DESC
+LIMIT 3;
---
+++ Queries/Query_3.sql
@@ -0,0 +1,17 @@
+
+SELECT 
+	pf.performer_name AS Performer,
+	e.festival_id AS Festival,
+	COUNT(*) AS warmup_count
+FROM 
+	Performance p
+JOIN
+	Performer pf ON p.performer_id = pf.performer_id
+JOIN 
+	Event e ON e.event_id=p.event_id
+WHERE 
+	p.type_id = 'warm-up'
+GROUP BY 
+	pf.performer_id, e.festival_id
+HAVING 
+	COUNT(*) >= 3
