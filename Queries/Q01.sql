09a21521fe6584ff929399431d5a2c68aab891fc
peri-7 <pericles.alexiou@gmail.com>
2025-05-01 23:11:38 +0300

    Update Q01.sql

--- Queries/Q01.sql
+++ Queries/Q01.sql
@@ -1,4 +1,4 @@
 SELECT YEAR(purchase_date) AS purchase_year, SUM(price) AS total_revenue, pay_method
 FROM TICKETS
-GROUP BY YEAR(purchase_date), pay_method
-ORDER BY purchase_year,paymethod
+GROUP BY purchase_year, pay_method
+ORDER BY purchase_year,pay_method;
