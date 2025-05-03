488810e364fd9b819d7154cbe878da4e9ff532da
peri-7 <pericles.alexiou@gmail.com>
2025-05-03 16:07:54 +0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -285,25 +285,18 @@ BEGIN
 
 	-- if it s a band performance
     ELSE
-		SELECT COUNT(*) INTO cnt
-		FROM (
-			SELECT P.performer_id, p.start_time, p.end_time, perf.artist_nband, perf.artist_id, perf.band_id
-			FROM Performance p JOIN Performer perf ON p.performer_id = perf.performer_id
-		) AS double_perf -- all the performances asociated with their performers
-		JOIN BandMembers bm
-		ON (
-			(double_perf.artist_nband = TRUE AND bm.artist_id = double_perf.artist_id) -- all the artists that perform and belong to a band 
-			OR (double_perf.artist_nband = FALSE AND bm.artist_id = (
-				SELECT artist_id FROM BandMembers WHERE band_id = double_perf.band_id
-				)
-			) -- all the artists whose band performs
-		) -- all the performing artists
-		WHERE bm.band_id = (SELECT band_id FROM Performer WHERE Performer_id = NEW.performer_id)
-		AND (
-		    (NEW.start_time BETWEEN double_perf.start_time AND double_perf.end_time) OR
-            (NEW.end_time BETWEEN double_perf.start_time AND double_perf.end_time) OR
-            (double_perf.start_time BETWEEN NEW.start_time AND NEW.end_time)
-		);
+    SELECT COUNT(*) INTO cnt
+    FROM Performance p
+    JOIN Performer perf ON p.performer_id = perf.performer_id
+    JOIN BandMembers bm1 ON perf.artist_nband = TRUE AND perf.artist_id = bm1.artist_id
+        OR (perf.artist_nband = FALSE AND perf.band_id = bm1.band_id)
+    JOIN BandMembers bm2 ON bm1.artist_id = bm2.artist_id
+    WHERE bm2.band_id = (SELECT band_id FROM Performer WHERE performer_id = NEW.performer_id)
+    AND (
+        (NEW.start_time BETWEEN p.start_time AND p.end_time) OR
+        (NEW.end_time BETWEEN p.start_time AND p.end_time) OR
+        (p.start_time BETWEEN NEW.start_time AND NEW.end_time)
+    );
     END IF;
 
     IF cnt > 0 THEN
@@ -323,36 +316,39 @@ AFTER INSERT ON Performer FOR EACH ROW
 BEGIN 
 	DECLARE id INT DEFAULT -1;
 	
-	IF ( NEW.artist_nband = TRUE)
-	THEN
-		SELECT a.artist_id INTO id
-		FROM Artist a 
-		WHERE a.artist_name = NEW.performer_name;
-		
-		IF ( id = -1 ) 
+    IF (NEW.performer_id = NULL)
+    THEN
+		IF ( NEW.artist_nband = TRUE)
 		THEN
-			SIGNAL SQLSTATE '45000'
-			SET MESSAGE_TEXT = 'Artist isnt registered yet.';
-		END IF;
+			SELECT a.artist_id INTO id
+			FROM Artist a 
+			WHERE a.artist_name = NEW.performer_name;
 		
-		UPDATE Performer
-			SET artist_id = id
-		WHERE performer_id = NEW.performer_id;
+			IF ( id = -1 ) 
+			THEN
+				SIGNAL SQLSTATE '45000'
+				SET MESSAGE_TEXT = 'Artist isnt registered yet.';
+			END IF;
+		
+			UPDATE Performer
+				SET artist_id = id
+			WHERE performer_id = NEW.performer_id;
 	
-	ELSE 
-		SELECT a.band_id INTO id
-		FROM Band b 
-		WHERE b.band_name = NEW.performer_name;
+		ELSE 
+			SELECT b.band_id INTO id
+			FROM Band b 
+			WHERE b.band_name = NEW.performer_name;
 
-		IF ( id = -1 ) 
-		THEN
-			SIGNAL SQLSTATE '45000'
-			SET MESSAGE_TEXT = 'Band isnt registered yet.';
+			IF ( id = -1 ) 
+			THEN
+				SIGNAL SQLSTATE '45000'
+				SET MESSAGE_TEXT = 'Band isnt registered yet.';
+			END IF;
+
+			UPDATE Performer
+				SET band_id = id
+			WHERE performer_id = NEW.performer_id;	
 		END IF;
-
-		UPDATE Performer
-			SET band_id = id
-		WHERE performer_id = NEW.performer_id;	
 	END IF;
 END//
 DELIMITER ;
