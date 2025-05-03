cea838726a0583069a5a63c9f44b83cc26fd26e0
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-03 15:07:26 +0300

    Update Q06.sql

--- Queries/Q06.sql
+++ Queries/Q06.sql
@@ -1,8 +1,12 @@
 SELECT 
 	t.event_id AS Event,
-	(r.voice + r.light_sound + r.stage_presence + r.organisation + r.overall)/5 AS Score
+	(AVG(r.voice) + AVG(r.light_sound) + AVG(r.stage_presence) + AVG(r.organisation) + AVG(r.overall))/5 AS Score
 FROM 
 	Ticket t 
 LEFT JOIN 
 	Review r ON r.EAN = t.EAN 
+JOIN
+	Performance p on p.performance_id = t.performance_id
+GROUP BY
+	t.event_id
 WHERE t.visitor_id = ? ;
