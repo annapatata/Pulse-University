2aa9d0b233adb68d32b35e0f3390001b30a0631d
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-03 16:09:05 +0300

    Update Q07.sql

--- Queries/Q07.sql
+++ Queries/Q07.sql
@@ -11,3 +11,17 @@ GROUP BY festival_id) AS avg_xp_per_festival
 ORDER BY average_xp ASC
 LIMIT 1;
 --subquery to calculate avg(xp) for tecnhical staff per festival
+
+
+--Find which festival has the lowest average of experience of technical staff
+
+SELECT festival_id FROM (
+SELECT festival_id, AVG(s.xp) AS average_xp
+FROM Event_P e
+JOIN Employment em ON em.event_id = e.event_id
+JOIN Staff s ON s.staff_id = em.staff_id 
+where s.role_id = 2
+GROUP BY festival_id) AS avg_xp_per_festival
+ORDER BY average_xp ASC
+LIMIT 1;
+--subquery to calculate avg(xp) for tecnhical staff per festival
