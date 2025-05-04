fb09e777ac64446e625e69ee1836308beda4465f
peri-7 <pericles.alexiou@gmail.com>
2025-05-04 14:15:33 +0300

    Update Q07.sql

--- Queries/Q07.sql
+++ Queries/Q07.sql
@@ -1,3 +1,16 @@
+-- CHECKED
+SELECT festival_id, average_xp AS avg_exp FROM (
+SELECT festival_id, AVG(s.xp) AS average_xp
+FROM Event_P e
+JOIN Employment em ON em.event_id = e.event_id
+JOIN Staff s ON s.staff_id = em.staff_id 
+where s.role_id = 2
+GROUP BY festival_id) AS avg_xp_per_festival
+ORDER BY average_xp ASC
+-- LIMIT 1;
+
+
+-- OLDER VERSION
 --Find which festival has the lowest average of experience of technical staff
 
 SELECT festival_id FROM (
@@ -12,14 +25,3 @@ ORDER BY average_xp ASC
 LIMIT 1;
 --subquery to calculate avg(xp) for tecnhical staff per festival
 
-
--- CHECKED
-SELECT festival_id, average_xp AS avg_exp FROM (
-SELECT festival_id, AVG(s.xp) AS average_xp
-FROM Event_P e
-JOIN Employment em ON em.event_id = e.event_id
-JOIN Staff s ON s.staff_id = em.staff_id 
-where s.role_id = 2
-GROUP BY festival_id) AS avg_xp_per_festival
-ORDER BY average_xp ASC
--- LIMIT 1;
