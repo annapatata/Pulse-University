d1d0b4f386ab94375a89adc4bc6fcb54a25e9265
peri-7 <pericles.alexiou@gmail.com>
2025-05-04 14:25:56 +0300

    Update Q08.sql

--- Queries/Q08.sql
+++ Queries/Q08.sql
@@ -1,10 +1,11 @@
-
-SELECT s.first_name, s.last_name
+-- CHECKED
+SELECT s.staff_id, s.first_name, s.last_name
 FROM Staff s 
+JOIN Role r ON s.role_id = r.role_id
 LEFT JOIN (
 	SELECT em.staff_id, ev.event_id
 	FROM Employment em 
 	JOIN Event_P ev ON em.event_id = ev.event_id
-	WHERE DATE(ev.start_time) = '2027-07-25'
+	WHERE DATE(ev.start_time) = '2026-08-23'
 ) w ON w.staff_id = s.staff_id
-WHERE s.role_name = 'supportive' AND w.event_id IS NULL;
+WHERE r.role_name = 'Auxiliary'  AND w.event_id IS NULL;
