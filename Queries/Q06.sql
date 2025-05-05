c8ed9aae2c3ba8c14e3ee19140291b31aee220bc
peri-7 <pericles.alexiou@gmail.com>
2025-05-05 23:06:53 +0300

    Update Q06.sql

--- Queries/Q06.sql
+++ Queries/Q06.sql
@@ -1,3 +1,5 @@
+-- CHECKED
+
 SELECT 
 	t.event_id AS Event,
 	(AVG(r.voice) + AVG(r.light_sound) + AVG(r.stage_presence) + AVG(r.organisation) + AVG(r.overall))/5 AS Score
@@ -5,6 +7,6 @@ FROM
 	Ticket t 
 LEFT JOIN 
 	Review r ON r.EAN = t.EAN 
-WHERE t.visitor_id = ?
+WHERE t.visitor_id = 1 AND t.activated = 1
 GROUP BY
-	t.event_id ;
+	t.event_id;
