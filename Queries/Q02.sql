6f312dbd4c3e0096d9fd0cdf95c76ca107bd71bb
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-06 13:09:57 +0300

    Update Q02.sql

--- Queries/Q02.sql
+++ Queries/Q02.sql
@@ -7,3 +7,12 @@ JOIN Subgenre s ON ps.subgenre_id = s.subgenre_id
  LEFT JOIN PerformerYears py ON p.performer_id = py.performer_id AND py.years_id = 2018
 WHERE s.genre_id = 'Rock'
 ORDER BY p.performer_name;
+
+--WITH VIEW
+SELECT DISTINCT p.performer_name,
+	 IF(py.years_id IS NOT NULL, 'YES', 'NO') AS Participated
+FROM Performer p 
+JOIN PerformerGenre pg ON p.performer_id = pg.performer_id
+LEFT JOIN PerformerYears py ON p.performer_id = py.performer_id AND py.years_id = 2018
+WHERE pg.genre = 'Rock'
+ORDER BY p.performer_name;
