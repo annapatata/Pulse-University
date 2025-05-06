8b7adf97734249170746ac840f479c95c04ce21a
annapatata <142344652+annapatata@users.noreply.github.com>
2025-05-06 12:20:25 +0300

    Update Q15.sql

--- Queries/Q15.sql
+++ Queries/Q15.sql
@@ -1,9 +1,10 @@
-SELECT visitor_name, performer_name, SUM(voice+overall) AS total_score
+--CHECKED
+SELECT v.first_name,v.last_name, performer_name, SUM(voice+overall) AS total_score
 FROM Review r
 JOIN Ticket t ON t.EAN = r.EAN
 JOIN Visitor v ON v.visitor_id = t.visitor_id
 JOIN Performance p ON p.performance_id = r.performance_id
 JOIN Performer per ON per.performer_id = p.performer_id
-GROUP BY visitor_name,performer_name
+GROUP BY v.last_name,performer_name
 ORDER BY total_score DESC 
 LIMIT 5;
