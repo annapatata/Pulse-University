ccea4a7575fe1a76e31c3dd96af9f8783fd033eb
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-06 13:20:41 +0300

    Update Q10.sql

--- Queries/Q10.sql
+++ Queries/Q10.sql
@@ -23,3 +23,23 @@ GROUP BY
 ORDER BY 
 	COUNT(*) DESC
 LIMIT 3;
+
+--WITH VIEW
+	
+SELECT 
+	p1.genre AS Genre_1,
+	p2.genre AS Genre_2,
+	COUNT(*) AS Artists
+FROM 
+     PerformerGenre p1
+JOIN 
+	PerformerGenre p2 
+	ON p1.performer_id = p2.performer_id
+WHERE
+	p1.genre < p2.genre
+GROUP BY 
+	p1.genre, p2.genre 
+ORDER BY 
+	COUNT(*) DESC
+LIMIT 3;
+
