31081c0df9ccf8efea854fc87ea1d88122457511
annapatata <142344652+annapatata@users.noreply.github.com>
2025-05-06 14:10:47 +0300

    Delete Queries/Query_6.sql

--- Queries/Query_6.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	t.event_id AS Event,
-	(r.voice + r.light_sound + r.stage_presence + r.organisation + r.overall)/5 AS Score
-FROM 
-	Ticket t 
-LEFT JOIN 
-	Review r ON r.EAN = t.EAN 
-WHERE t.visitor_id = ? ;
