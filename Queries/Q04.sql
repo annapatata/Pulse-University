0b43d9ae5cdcc63cbc93e22ab82536bd1268cf43
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-06 14:01:19 +0300

    Update Q04.sql

--- Queries/Q04.sql
+++ Queries/Q04.sql
@@ -4,3 +4,9 @@ FROM REVIEW r
 JOIN Performance p ON p.performance_id = r.performance_id
 JOIN Performer per ON per.performer_id = p.performer_id
 WHERE per.performer_name='Taylor Swift';
+
+-- VIEW
+SELECT performer_name, AVG(voice) as avg_voice , AVG(overall) as avg_overall 
+FROM REVIEW r 
+JOIN PerformerPerformance p ON p.performance_id = r.performance_id
+WHERE p.performer='Taylor Swift';
