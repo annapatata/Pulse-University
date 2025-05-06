aea162f23d76ac97607223c32ec0311ccb176b94
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-06 13:53:58 +0300

    Update Q03.sql

--- Queries/Q03.sql
+++ Queries/Q03.sql
@@ -10,3 +10,15 @@ JOIN Event_P e ON e.event_id=p.event_id
 WHERE p.type_id = 'warm-up'
 GROUP BY pf.performer_id, e.festival_id
 HAVING COUNT(*) >= 3
+
+-- VIEWS
+
+SELECT 
+	p.performer AS Performer,
+	e.festival_id AS Festival,
+	COUNT(*) AS warmup_count
+FROM PerformerPerformance p
+JOIN Event_P e ON e.event_id=p.Event
+WHERE p.Type = 'warm-up'
+GROUP BY p.performer_id, e.festival_id
+HAVING COUNT(*) >= 3;
