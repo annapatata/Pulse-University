c1998ffe67627bf8b129650c9961796e720fc0eb
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-06 12:14:16 +0300

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,9 +1,11 @@
 -- VIEWS
+DROP VIEW IF EXISTS PerformerGenre;
 CREATE VIEW PerformerGenre AS 
 SELECT DISTINCT ps.performer_id AS performer_id, s.genre_id AS genre
 FROM PerformerSubgenre ps 
 JOIN Subgenre s ON s.subgenre_id = ps.subgenre_id;
 
+DROP VIEW IF EXISTS EventStaff;
 CREATE VIEW EventStaff AS
 SELECT e.festival_id AS Festival, e.festival_day AS Festival_day, s.role_id AS Role, s.xp AS xp
 FROM 
@@ -13,6 +15,7 @@ JOIN
 JOIN 
 	Staff s ON s.staff_id = es.staff_id;
 
+DROP VIEW IF EXISTS PerformerPerformance;
 CREATE VIEW PerformerPerformance AS
 SELECT pf.performer_name AS Performer, pf.performer_id AS Performer_id, p.event_id AS Event, p.type_id AS Type 
 FROM Performance p 
