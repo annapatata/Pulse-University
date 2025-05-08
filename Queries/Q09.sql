4d4feeaad12c726362d52fdf4dcacb4022d8d641
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-08 19:10:11 +0300

    Update Q09.sql

--- Queries/Q09.sql
+++ Queries/Q09.sql
@@ -27,22 +27,3 @@ JOIN ec AS ec2
    AND ec1.eventcounts = ec2.eventcounts;
 
 
--- CHECKED
-SELECT 
-	e.festival_id AS Festival
-	EventCounts.visitor_id AS Visitor
-	EventCounts.Event_number AS Events
-	
-FROM(
-	SELECT
-		t.visitor_id,
-		COUNT(*) AS Event_number
-	FROM
-		Ticket t
-	JOIN
-		Event_P e ON t.event_id = e.event_id
-	GROUP BY
-		t.visitor_id
-)AS EventCounts
-WHERE
-	EventCounts.Event_number > 3;
