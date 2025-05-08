fe6e9f2513f3d693babfbc680b460e14d17c2ed5
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-08 17:40:48 +0300

    Add files via upload

---
+++ Queries/Query_9.sql
@@ -0,0 +1,17 @@
+SELECT 
+	t.visitor_id AS Visitor
+FROM(
+	SELECT
+		t.visitor_id,
+		COUNT(*) AS Event_number
+	FROM
+		Ticket t
+	JOIN
+		Event e ON t.event_id = e.event_id
+	WHERE 
+		e.start_time BETWEEN DATE_SUB(CURDATE(), INTERVAL 1 YEAR) AND CURDATE()
+	GROUP BY
+		t.visitor_id
+)AS EventCounts
+WHERE
+	EventCounts.Event_number = ?;
