33a0efd67ca3931c5a528d43bf5bd09bd0bef094
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-08 22:33:44 +0300

    Update views.sql

--- views.sql
+++ views.sql
@@ -20,3 +20,16 @@ CREATE VIEW PerformerPerformance AS
 SELECT pf.performer_name AS Performer, pf.performer_id AS Performer_id, p.event_id AS Event, p.performance_id AS performance, p.type_id AS Type 
 FROM Performance p 
 JOIN Performer pf ON pf.performer_id = p.performer_id;
+
+DROP VIEW IF EXISTS VisitorTicket;
+CREATE VIEW VisitorTicket AS
+SELECT v.first_name AS First_Name, v.last_name AS Last_Name, v.visitor_id AS Visitor_id, e.festival_id AS Festival e.festival_day AS Festival_day, t.EAN AS EAN
+FROM Visitor v
+JOIN Ticket t ON t.visitor_id = v.visitor_id
+JOIN Event_P e ON t.event_id = e.event_id
+
+DROP VIEW IF EXISTS EventPerformer;
+CREATE VIEW EventPerformer AS
+SELECT e.festival_id AS Festival, e.festival_day AS Festival_day, p.performer
+FROM Event_P e
+JOIN PerformerPerformance p ON p.Event = e.event_id
