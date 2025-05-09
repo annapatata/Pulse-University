95dc97b61d766be36043b79c466a9d030068248c
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-09 14:14:34 +0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -586,19 +586,6 @@ BEGIN
 	 END$$
 DELIMITER ;
 
-DROP TRIGGER IF EXISTS purchase;
-DELIMITER $$
-CREATE TRIGGER purchase
-AFTER UPDATE ON Ticket
-FOR EACH ROW
-BEGIN
-	IF OLD.visitor_id != NEW.visitor_id THEN
-		DELETE FROM Buyer WHERE (EAN = NEW.EAN OR (visitor_id = NEW.visitor_id AND event_id = NEW.event_id));
-		DELETE FROM Resale_queue WHERE EAN = NEW.EAN;
-	END IF;
-        END$$
-DELIMITER;
-
 DROP TRIGGER IF EXISTS review_attended_performance;
 DELIMITER $$
 CREATE TRIGGER review_attended_performance
