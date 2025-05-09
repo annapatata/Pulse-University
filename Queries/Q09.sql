ca5fc3c301676f31334e2b8248ac1a292802c7a5
peri-7 <pericles.alexiou@gmail.com>
2025-05-09 15:22:40 +0300

    Update Q09.sql

--- Queries/Q09.sql
+++ Queries/Q09.sql
@@ -1,29 +1,18 @@
---new version
-WITH ec AS (
-    SELECT
-        t.visitor_id AS visitor,
-        e.festival_id AS fest_year, 
-        COUNT(*) AS eventcounts
-    FROM
-        Ticket t
-    JOIN
-        Event_P e ON t.event_id = e.event_id 
-    WHERE
-        t.activated = 1
-    GROUP BY
-        t.visitor_id, e.festival_id
-    HAVING COUNT(*) > 3
-)
-
+-- CHECKED
 SELECT 
-    ec1.visitor,
-    ec1.fest_year AS festival1,
-    ec2.fest_year AS festival2,
-    ec1.eventcounts AS EventCounts
-FROM ec AS ec1 
-JOIN ec AS ec2 
-    ON ec2.fest_year = ec1.fest_year + INTERVAL 1 YEAR 
-   AND ec1.visitor = ec2.visitor 
-   AND ec1.eventcounts = ec2.eventcounts;
-
-
+	festival_id AS Festival_year, visitor_id AS Visitor, Event_number
+FROM(
+	SELECT
+		e.festival_id,
+		t.visitor_id,
+		COUNT(*) AS Event_number
+	FROM
+		Ticket t
+	JOIN
+		Event_p e ON t.event_id = e.event_id
+	GROUP BY
+		e.festival_id, t.visitor_id
+	HAVING 
+		Event_number > 3
+)AS EventCounts
+ORDER BY Festival_year, Event_number
