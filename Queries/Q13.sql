3ddd3492e31f88d53233bfc58200ced1c06d340f
peri-7 <pericles.alexiou@gmail.com>
2025-05-09 15:04:29 +0300

    Update Q13.sql

--- Queries/Q13.sql
+++ Queries/Q13.sql
@@ -1,6 +1,5 @@
---Find the performers that have participated in 3 different continents
---CHECKED
-SELECT performer_name 
+-- CHECKED
+SELECT performer_name, COUNT(DISTINCT c.continent_id) AS num_continents
 FROM Performer per
 JOIN Performance p ON per.performer_id = p.performer_id
 JOIN Event_P e ON e.event_id = p.event_id
@@ -8,8 +7,5 @@ JOIN Festival f ON f.festival_id = e.festival_id
 JOIN Location l ON l.festival_id = f.festival_id
 JOIN Continent c ON c.continent_id = l.continent_id
 GROUP BY performer_name
-HAVING COUNT(DISTINCT continent_name) >=3
+HAVING COUNT(DISTINCT c.continent_id) >=3
 ORDER BY performer_name ASC;
-
-
-
