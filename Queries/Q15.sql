62841bce1d0fe3c2ad4675e6c6b922ba69113b1b
peri-7 <pericles.alexiou@gmail.com>
2025-05-09 15:12:56 +0300

    Update Q15.sql

--- Queries/Q15.sql
+++ Queries/Q15.sql
@@ -5,7 +5,7 @@ JOIN Ticket t ON t.EAN = r.EAN
 JOIN Visitor v ON v.visitor_id = t.visitor_id
 JOIN Performance p ON p.performance_id = r.performance_id
 JOIN Performer per ON per.performer_id = p.performer_id
-GROUP BY v.last_name,performer_name
+GROUP BY v.first_name, v.last_name,performer_name
 ORDER BY total_score DESC 
 LIMIT 5;
 
@@ -15,7 +15,6 @@ FROM Review r
 JOIN Ticket t ON t.EAN = r.EAN
 JOIN Visitor v ON v.visitor_id = t.visitor_id
 JOIN PerformerPerformance p ON r.performance_id = p.performance
-GROUP BY v.last_name, p.performer
+GROUP BY v.first_name, v.last_name, p.performer
 ORDER BY total_score DESC 
 LIMIT 5;
-
