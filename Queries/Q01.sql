7ee766439edb2e7a4e3ee63534f5e5a5d9e15c22
peri-7 <pericles.alexiou@gmail.com>
2025-05-11 14:11:22 +0300

    Update Q01.sql

--- Queries/Q01.sql
+++ Queries/Q01.sql
@@ -1,5 +1,14 @@
 -- CHECKED
-SELECT YEAR(purchase_date) AS purchase_year, SUM(price) AS total_revenue, pay_method
-FROM Ticket
-GROUP BY purchase_year, pay_method
-ORDER BY purchase_year,pay_method;
+SELECT 
+    YEAR(t.purchase_date) AS year,
+    SUM(t.price) AS Total,
+    SUM(CASE WHEN t.pay_method = 'Credit' THEN t.price ELSE 0 END) AS Credit,
+    SUM(CASE WHEN t.pay_method = 'Debit' THEN t.price ELSE 0 END) AS Debit,
+    SUM(CASE WHEN t.pay_method = 'Cash' THEN t.price ELSE 0 END) AS Cash,
+    SUM(CASE WHEN t.pay_method = 'PayPal' THEN t.price ELSE 0 END) AS PayPal
+FROM 
+    Ticket t
+GROUP BY 
+    YEAR(t.purchase_date)
+ORDER BY 
+    YEAR(t.purchase_date);
