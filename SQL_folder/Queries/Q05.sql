37449ee31e4a99b6b7b6c23b73c0756648cd165b
annapatata <142344652+annapatata@users.noreply.github.com>
2025-05-13 22:58:16 +0300

    Update Q05.sql

--- SQL_folder/Queries/Q05.sql
+++ SQL_folder/Queries/Q05.sql
@@ -1,4 +1,4 @@
-SELECT stage_name, participations
-FROM Artist  
-WHERE birthDate >= YEAR(CURDATE()) - 30
-ORDER BY participations DESC;
+SELECT a.stage_name, a.participations
+FROM Artist a force index (idx_artist_birthDate)
+WHERE a.birthDate >= DATE_SUB(CURDATE(), INTERVAL 30 YEAR)
+ORDER BY a.participations DESC;
