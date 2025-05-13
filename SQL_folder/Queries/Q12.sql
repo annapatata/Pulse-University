68ef94f50b3430f78fc63c00bf245ff8fa4b4ef6
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-14 00:13:36 +0300

    Update Q12.sql

--- SQL_folder/Queries/Q12.sql
+++ SQL_folder/Queries/Q12.sql
@@ -1,17 +1,13 @@
 SELECT
-	e.festival_id,
-	e.festival_day AS Festival_day,
-	COUNT(CASE WHEN s.role_id = 1 THEN 1 END) AS Auxiliary,
-	COUNT(CASE WHEN s.role_id = 3 THEN 1 END) AS Security,    
-	COUNT(CASE WHEN s.role_id = 2 THEN 1 END) AS Technician
+	festival,
+	festival_day,
+	COUNT(CASE WHEN role = 1 THEN 1 END) AS Auxiliary,
+	COUNT(CASE WHEN role= 3 THEN 1 END) AS Security,    
+	COUNT(CASE WHEN role = 2 THEN 1 END) AS Technician
 FROM 
-	Event_P e
-JOIN 
-	Employment es ON e.event_id = es.event_id
-JOIN 
-	Staff s ON s.staff_id = es.staff_id
+	EventStaff
 -- WHERE e.festival_id = 2018 -- for a specific year
 GROUP BY 
-	e.festival_id, e.festival_day
+	festival, festival_day
 ORDER BY 
-	e.festival_id, e.festival_day;
+	festival, festival_day;
