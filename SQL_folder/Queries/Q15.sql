c2952b1f3f2044edb32f585c4baa3286ba68d761
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-13 22:49:48 +0300

    Update Q15.sql

--- SQL_folder/Queries/Q15.sql
+++ SQL_folder/Queries/Q15.sql
@@ -1,8 +1,8 @@
-SELECT v.first_name,v.last_name, p.performer, SUM(voice+overall) AS total_score
+SELECT v.visitor_id, v.first_name,v.last_name, p.performer, SUM(voice+overall) AS total_score
 FROM Review r
 JOIN Ticket t ON t.EAN = r.EAN
 JOIN Visitor v ON v.visitor_id = t.visitor_id
 JOIN PerformerPerformance p ON r.performance_id = p.performance
-GROUP BY v.first_name, v.last_name, p.performer
+GROUP BY v.visitor_id, p.performer
 ORDER BY total_score DESC 
 LIMIT 5;
