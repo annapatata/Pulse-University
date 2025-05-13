5a19c1b0c9cb972f67b2f2eaa8b8f1a5af5ddd84
peri-7 <pericles.alexiou@gmail.com>
2025-05-13 21:39:03 +0300

    Update Q14.sql

--- Queries/Q14.sql
+++ Queries/Q14.sql
@@ -1,39 +1,3 @@
--- CHECKED 
-
-WITH fgp1 AS (
-	SELECT
-		festival,
-		genre,
-		COUNT(*) AS performances
-	FROM
-		(SELECT DISTINCT
-			p.performance_id AS performance,
-			e.festival_id AS festival,
-			s.genre_id AS genre
-		FROM
-			Event_P e 
-		JOIN Performance p ON e.event_id = p.event_id
-		JOIN PerformerSubgenre ps ON p.performer_id = ps.performer_id
-		JOIN Subgenre s ON s.subgenre_id = ps.subgenre_id
-		)AS fgp
-	GROUP BY 
-		festival,genre
-	HAVING COUNT(*) >=3
-)
-
-SELECT DISTINCT 
-	f1.genre,
-	f1.festival,
-	f2.festival,
-	f1.performances
-FROM fgp1 as f1
-JOIN fgp1 AS f2 ON
-	f2.festival = f1.festival + 1 	AND
-	f1.genre = f2.genre AND
-	f1.performances = f2.performances;
-
--- WITH VIEW
-
 WITH fgp1 AS (
 	SELECT
 		festival,
