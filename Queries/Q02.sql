1420533748f44131d7897092aa0a4bc050b2fd7e
peri-7 <pericles.alexiou@gmail.com>
2025-05-13 21:21:32 +0300

    Update Q02.sql

--- Queries/Q02.sql
+++ Queries/Q02.sql
@@ -1,14 +1,3 @@
--- CHECKED 
-SELECT DISTINCT p.performer_name,
-	 IF(py.years_id IS NOT NULL, 'YES', 'NO') AS Participated
-FROM Performer p 
-JOIN PerformerSubgenre ps ON p.performer_id = ps.performer_id
-JOIN Subgenre s ON ps.subgenre_id = s.subgenre_id
- LEFT JOIN PerformerYears py ON p.performer_id = py.performer_id AND py.years_id = 2018
-WHERE s.genre_id = 'Pop'
-ORDER BY p.performer_name;
-
--- WITH VIEW
 SELECT DISTINCT p.performer_name,
 	 IF(py.years_id IS NOT NULL, 'YES', 'NO') AS Participated
 FROM Performer p 
