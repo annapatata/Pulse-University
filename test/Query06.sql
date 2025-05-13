016bd82a89d103ddb4cf2edc914f7c17425e3086
peri-7 <pericles.alexiou@gmail.com>
2025-05-13 21:11:39 +0300

    Create Query06.sql

---
+++ test/Query06.sql
@@ -0,0 +1,12 @@
+-- CHECKED
+
+SELECT 
+	t.event_id AS Event,
+	(AVG(r.voice) + AVG(r.light_sound) + AVG(r.stage_presence) + AVG(r.organisation) + AVG(r.overall))/5 AS Score
+FROM 
+	Ticket t 
+LEFT JOIN 
+	Review r ON r.EAN = t.EAN 
+WHERE t.visitor_id = 1 AND t.activated = 1
+GROUP BY
+	t.event_id;
