d6ae6edbb88bda37934b4caada3c4966e5a4990e
peri-7 <pericles.alexiou@gmail.com>
2025-05-13 21:09:39 +0300

    Create Query02.sql

---
+++ test/Query02.sql
@@ -0,0 +1,18 @@
+-- CHECKED 
+SELECT DISTINCT p.performer_name,
+	 IF(py.years_id IS NOT NULL, 'YES', 'NO') AS Participated
+FROM Performer p 
+JOIN PerformerSubgenre ps ON p.performer_id = ps.performer_id
+JOIN Subgenre s ON ps.subgenre_id = s.subgenre_id
+ LEFT JOIN PerformerYears py ON p.performer_id = py.performer_id AND py.years_id = 2018
+WHERE s.genre_id = 'Pop'
+ORDER BY p.performer_name;
+
+-- WITH VIEW
+SELECT DISTINCT p.performer_name,
+	 IF(py.years_id IS NOT NULL, 'YES', 'NO') AS Participated
+FROM Performer p 
+JOIN PerformerGenre pg ON p.performer_id = pg.performer_id
+LEFT JOIN PerformerYears py ON p.performer_id = py.performer_id AND py.years_id = 2018
+WHERE pg.genre = 'Pop'
+ORDER BY p.performer_name;
