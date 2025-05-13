f7d6cc880217db524ea6f6c82b0dfa90cc54918d
peri-7 <pericles.alexiou@gmail.com>
2025-05-13 21:09:13 +0300

    Create Query01.sql

---
+++ test/Query01.sql
@@ -0,0 +1,14 @@
+-- CHECKED
+SELECT 
+    YEAR(t.purchase_date) AS year,
+    SUM(t.price) AS Total,
+    SUM(CASE WHEN t.pay_method = 'Credit' THEN t.price ELSE 0 END) AS Credit,
+    SUM(CASE WHEN t.pay_method = 'Debit' THEN t.price ELSE 0 END) AS Debit,
+    SUM(CASE WHEN t.pay_method = 'Cash' THEN t.price ELSE 0 END) AS Cash,
+    SUM(CASE WHEN t.pay_method = 'PayPal' THEN t.price ELSE 0 END) AS PayPal
+FROM 
+    Ticket t
+GROUP BY 
+    YEAR(t.purchase_date)
+ORDER BY 
+    YEAR(t.purchase_date);
