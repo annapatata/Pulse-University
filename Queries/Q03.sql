f5666b41d41df4479865f89c641f5985ff093605
peri-7 <pericles.alexiou@gmail.com>
2025-05-13 21:21:49 +0300

    Update Q03.sql

--- Queries/Q03.sql
+++ Queries/Q03.sql
@@ -1,18 +1,3 @@
--- CHECKED
-
-SELECT 
-	pf.performer_name AS Performer,
-	e.festival_id AS Festival,
-	COUNT(*) AS warmup_count
-FROM Performance p
-JOIN Performer pf ON p.performer_id = pf.performer_id
-JOIN Event_P e ON e.event_id=p.event_id
-WHERE p.type_id = 'warm-up'
-GROUP BY pf.performer_id, e.festival_id
-HAVING COUNT(*) >= 3
-
--- VIEWS
-
 SELECT 
 	p.performer AS Performer,
 	e.festival_id AS Festival,
