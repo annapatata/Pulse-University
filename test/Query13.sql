8f5dc0e4b01be787faff02a260e4bc93fd8979de
peri-7 <pericles.alexiou@gmail.com>
2025-05-13 21:19:35 +0300

    Create Query13.sql

---
+++ test/Query13.sql
@@ -0,0 +1,11 @@
+-- CHECKED
+SELECT performer_name, COUNT(DISTINCT c.continent_id) AS num_continents
+FROM Performer per
+JOIN Performance p ON per.performer_id = p.performer_id
+JOIN Event_P e ON e.event_id = p.event_id
+JOIN Festival f ON f.festival_id = e.festival_id 
+JOIN Location l ON l.festival_id = f.festival_id
+JOIN Continent c ON c.continent_id = l.continent_id
+GROUP BY performer_name
+HAVING COUNT(DISTINCT c.continent_id) >=3
+ORDER BY performer_name ASC;
