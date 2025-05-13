0e6ae96d137d31193e87bbd65d00e78ddabbf225
annapatata <142344652+annapatata@users.noreply.github.com>
2025-05-13 21:29:17 +0300

    Update Q05.sql

--- Queries/Q05.sql
+++ Queries/Q05.sql
@@ -1,4 +1,4 @@
 SELECT a.stage_name, a.participations
 FROM Artist a 
-WHERE TIMESTAMPDIFF(YEAR, a.birthDate, CURRENT_DATE) < 30
-ORDER BY a.participations DESC; 
+WHERE a.birthDate >= DATE_SUB(CURDATE(), INTERVAL 30 YEAR)
+ORDER BY a.participations DESC;
