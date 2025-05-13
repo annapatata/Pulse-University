10e865efc76cc62a7bdbaab982c06cbe71d21b7e
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-14 00:22:01 +0300

    Update Q07.sql

--- SQL_folder/Queries/Q07.sql
+++ SQL_folder/Queries/Q07.sql
@@ -1,9 +1,7 @@
 SELECT festival_id, average_xp AS avg_exp FROM (
-SELECT festival_id, AVG(s.xp) AS average_xp
-FROM Event_P e
-JOIN Employment em ON em.event_id = e.event_id
-JOIN Staff s ON s.staff_id = em.staff_id 
-where s.role_id = 2
-GROUP BY festival_id) AS avg_xp_per_festival
+SELECT festival, AVG(xp) AS average_xp
+FROM EventStaff
+where Role = 2
+GROUP BY festival) AS avg_xp_per_festival
 ORDER BY average_xp ASC
 LIMIT 1;
