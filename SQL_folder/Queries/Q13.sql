c602a763ebf6390649f6a0f252a947905a8ddab9
sewingmachine18 <131910560+sewingmachine18@users.noreply.github.com>
2025-05-14 00:05:51 +0300

    Update Q13.sql

--- SQL_folder/Queries/Q13.sql
+++ SQL_folder/Queries/Q13.sql
@@ -1,10 +1,10 @@
-SELECT performer_name, COUNT(DISTINCT c.continent_id) AS num_continents
-FROM Performer per
-JOIN Performance p ON per.performer_id = p.performer_id
-JOIN Event_P e ON e.event_id = p.event_id
+-- CHECKED
+SELECT performer_id, performer, COUNT(DISTINCT c.continent_id) AS num_continents
+FROM PerformerPerformance p
+JOIN Event_P e ON e.event_id = p.event
 JOIN Festival f ON f.festival_id = e.festival_id 
 JOIN Location l ON l.festival_id = f.festival_id
 JOIN Continent c ON c.continent_id = l.continent_id
-GROUP BY performer_name
+GROUP BY performer_id
 HAVING COUNT(DISTINCT c.continent_id) >=3
-ORDER BY performer_name ASC;
+ORDER BY num_continents DESC;
