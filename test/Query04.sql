6cbd33a22f53d78c98468da242a35e2c986f9f6b
peri-7 <pericles.alexiou@gmail.com>
2025-05-13 21:10:29 +0300

    Create Query04.sql

---
+++ test/Query04.sql
@@ -0,0 +1,13 @@
+
+-- CHECKED
+SELECT performer_name,AVG(voice) as avg_voice , AVG(overall) as avg_overall 
+FROM REVIEW r 
+JOIN Performance p ON p.performance_id = r.performance_id
+JOIN Performer per ON per.performer_id = p.performer_id
+WHERE per.performer_name='Taylor Swift';
+
+-- VIEW
+SELECT p.performer AS performer, AVG(voice) as avg_voice , AVG(overall) as avg_overall 
+FROM REVIEW r 
+JOIN PerformerPerformance p ON p.performance = r.performance_id
+WHERE p.performer='Taylor Swift';
